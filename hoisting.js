a7409ee894d84e0d7e9ce9b5c29c75ad2e2affd8
Andrea Abshire <sashady1488@gmail.com>
2018-11-02 22:57:13 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -10,7 +10,7 @@ function callMe() {
 
 
 function crazy() {
-  // fix the code in here:
+  var thisIsCrazy;
   thisIsCrazy();
 
   var thisIsCrazy = function (){
@@ -19,13 +19,13 @@ function crazy() {
 }
 
 function sayMyName() {
-  // fix the code in here:
+  var name = "Kristin";
   var name = "Cricky";
 
   sayMy();
 
   function sayMy() {
     console.log(name);
-    var name = "Kristin";
+    
   }
 }
