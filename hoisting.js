c17f1354edb16a19ad9a27198253daf801b9d7d2
Andrea Abshire <sashady1488@gmail.com>
2018-11-02 22:59:55 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,15 +1,16 @@
+var lyric = "maybe";
 function callMe() {
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
+
 }
 
 
 function crazy() {
-  // fix the code in here:
+  var thisIsCrazy;
   thisIsCrazy();
 
   var thisIsCrazy = function (){
@@ -18,13 +19,13 @@ function crazy() {
 }
 
 function sayMyName() {
-  // fix the code in here:
+  var name = "Kristin";
   var name = "Cricky";
 
   sayMy();
 
   function sayMy() {
     console.log(name);
-    var name = "Kristin";
+
   }
 }
